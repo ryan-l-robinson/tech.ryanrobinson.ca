f0e119a245a25b5e9cdb108ddface90b04ff1aab
Ryan Robinson <ryan@ryanrobinson.ca>
2025-04-16 17:03:21 -0400

    Added page title to title attribute

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,7 +1,7 @@
 export default {
 	title: "Ryan Robinson Technology",
 	url: "https://tech.ryanrobinson.ca/",
-	language: "en",
+	language: "en-ca",
 	site_description: "Technologist writing about web development, Microsoft 365, and more.",
 	author: {
 		name: "Ryan Robinson",
