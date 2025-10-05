04bd159c7b0f7510901cdb99921eafb89cbdf0d5
Ryan Robinson <ryan@ryanrobinson.ca>
2025-10-05 15:02:59 -0400

    Cleaned up code a little

--- eleventy.config.js
+++ eleventy.config.js
@@ -13,9 +13,6 @@ export default function (eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig
-		.addPassthroughCopy({
-			"./11ty-theme/public/": "/"
-		})
 		.addPassthroughCopy("./public")
 		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl")
 		.addPassthroughCopy("./CNAME");
@@ -98,43 +95,42 @@ export default function (eleventyConfig) {
 	  execSync(`npx pagefind --site _site`, { encoding: 'utf-8' })
 	})
 
-};
+	return {
+		// Control which files Eleventy will process
+		// e.g.: *.md, *.njk, *.html, *.liquid
+		templateFormats: [
+			"md",
+			"njk",
+			"html",
+			"liquid",
+			"11ty.js",
+		],
 
-export const config = {
-	// Control which files Eleventy will process
-	// e.g.: *.md, *.njk, *.html, *.liquid
-	templateFormats: [
-		"md",
-		"njk",
-		"html",
-		"liquid",
-		"11ty.js",
-	],
+		// Pre-process *.md files with: (default: `liquid`)
+		markdownTemplateEngine: "njk",
 
-	// Pre-process *.md files with: (default: `liquid`)
-	markdownTemplateEngine: "njk",
+		// Pre-process *.html files with: (default: `liquid`)
+		htmlTemplateEngine: "njk",
 
-	// Pre-process *.html files with: (default: `liquid`)
-	htmlTemplateEngine: "njk",
+		// These are all optional:
+		dir: {
+			input: "content",          // default: "."
+			includes: "../11ty-theme/_includes",  // default: "_includes" (`input` relative)
+			data: "../_data",          // default: "_data" (`input` relative)
+			output: "_site"
+		},
 
-	// These are all optional:
-	dir: {
-		input: "content",          // default: "."
-		includes: "../11ty-theme/_includes",  // default: "_includes" (`input` relative)
-		data: "../_data",          // default: "_data" (`input` relative)
-		output: "_site"
-	},
+		// -----------------------------------------------------------------
+		// Optional items:
+		// -----------------------------------------------------------------
 
-	// -----------------------------------------------------------------
-	// Optional items:
-	// -----------------------------------------------------------------
+		// If your site deploys to a subdirectory, change `pathPrefix`.
+		// Read more: https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
 
-	// If your site deploys to a subdirectory, change `pathPrefix`.
-	// Read more: https://www.11ty.dev/docs/config/#deploy-to-a-subdirectory-with-a-path-prefix
+		// When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
+		// it will transform any absolute URLs in your HTML to include this
+		// folder name and does **not** affect where things go in the output folder.
 
-	// When paired with the HTML <base> plugin https://www.11ty.dev/docs/plugins/html-base/
-	// it will transform any absolute URLs in your HTML to include this
-	// folder name and does **not** affect where things go in the output folder.
-
-	// pathPrefix: "/",
+		// pathPrefix: "/",
+	};
 };
