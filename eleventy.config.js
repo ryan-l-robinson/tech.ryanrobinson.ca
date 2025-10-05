ce64ab8e62ef5c9b54ffe51f7f836aca20e20b23
Ryan Robinson <ryan@ryanrobinson.ca>
2025-10-05 15:17:05 -0400

    Fixed passthrough for favicon

--- eleventy.config.js
+++ eleventy.config.js
@@ -13,7 +13,7 @@ export default function (eleventyConfig) {
 	// Copy the contents of the `public` folder to the output folder
 	// For example, `./public/css/` ends up in `_site/css/`
 	eleventyConfig
-		.addPassthroughCopy("./public")
+		.addPassthroughCopy({ "public/": "/" })
 		.addPassthroughCopy("./content/feed/pretty-atom-feed.xsl")
 		.addPassthroughCopy("./CNAME");
 
