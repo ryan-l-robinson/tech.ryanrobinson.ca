50696d13b45a9b9a04236abeec7c520c282f8c57
Ryan Robinson <ryan@ryanrobinson.ca>
2025-12-07 16:00:25 -0500

    Differentiated icon by site

--- _data/metadata.js
+++ _data/metadata.js
@@ -1,10 +1,12 @@
 export default {
 	title: "Ryan Robinson Technology",
 	url: "https://tech.ryanrobinson.ca/",
+	link_icon: "tech-blog",
 	language: "en-ca",
-	site_description: "Technologist writing about web development, Microsoft 365, and more.",
+	site_description:
+		"Technologist writing about web development, Microsoft 365, and more.",
 	author: {
 		name: "Ryan Robinson",
-		url: "https://ryanrobinson.ca"
-	}
-}
+		url: "https://ryanrobinson.ca",
+	},
+};
